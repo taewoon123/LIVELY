c0ab52f3bc7d8650ce54c6f6f7c6ee433a0dff16
bonzonkim <flrhyme9@gmail.com>
2023-04-19 22:57:10 +0900

    header,footer 테스트용 'app.test.controller>TestController.java'생성,bootstrap css,js 등 common폴더안에 추가

---
+++ src/main/java/com/lively/app/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.lively.app.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//header,footer Test용
+@Controller
+public class TestController {
+    @RequestMapping("header")
+    public String header() {
+        return "common/header";
+    }
+    @RequestMapping("footer")
+    public String footer() {
+        return "common/footer";
+    }
+}

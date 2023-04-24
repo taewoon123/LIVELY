c5113591e6cc4dff332fda887c39bb8379799015
taewoon123 <rkdxodns5623@naver.com>
2023-04-24 17:51:52 +0900

    챗컨트롤러&친구컨트롤러 이름변경

--- src/main/java/com/lively/chat/controller/ChatController.java
+++ src/main/java/com/lively/chat/controller/ChatController.java
@@ -1,4 +1,4 @@
-package com.lively.app.chat.controller;
+package com.lively.chat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/lively/friend/controller/FriendController.java
+++ src/main/java/com/lively/friend/controller/FriendController.java
@@ -1,4 +1,4 @@
-package com.lively.app.friend.controller;
+package com.lively.friend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

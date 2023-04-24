594b5abc1a5cce0e65fc7c82513a1977bfeab247
sopiabung <sopia0056@naver.com>
2023-04-24 15:41:38 +0900

    member join,login - controller,jsp,css 생성

---
+++ src/main/java/com/lively/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.lively.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
+
+public class MemberController {
+
+	@GetMapping("login")
+	public void login() {}
+	
+	@GetMapping("join")
+	public void join() {}
+	
+}

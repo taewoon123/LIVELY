7b354caaf3d8efeb60f9f2eb4994393924cb70cd
sopiabung <sopia0056@naver.com>
2023-05-06 00:28:29 +0900

    0505 - 01 : query UI 1차 완료

---
+++ src/main/webapp/resources/js/query/query-list.js
@@ -0,0 +1,8 @@
+// //행 클릭했을 때, 상세조회
+// const table = document.querySelector('main > table > tbody');
+// table.addEventListener("click",(event)=>{
+//     const no = event.target.parentNode.children[0].innerText;
+//     location.href = '${rootContext}/query/detail?no=' + no;
+// });
+
+

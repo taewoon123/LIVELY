c15344c0fd70344ac329d4d24ed9ac9a28153bd6
sopiabung <sopia0056@naver.com>
2023-06-01 00:12:27 +0900

    060102 회원가입 시 비밀번호 암호화, 로그인시 복호화 작업완료. mapper는 줄정렬함.

--- src/main/java/com/lively/member/service/MemberService.java
+++ src/main/java/com/lively/member/service/MemberService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,19 +21,28 @@ public class MemberService {
 
 	private final SqlSessionTemplate sst;
 	private final MemberDao dao;
+	private final BCryptPasswordEncoder enc;
 
 	@Autowired
-	public MemberService(SqlSessionTemplate sst, MemberDao dao) {
+	public MemberService(SqlSessionTemplate sst, MemberDao dao, BCryptPasswordEncoder enc) {
 		this.sst = sst;
 		this.dao = dao;
+		this.enc = enc;
 	}
 
-	//회원가입
+	// 회원가입
 	public int join(MemberVo vo) throws Exception {
+
+		String pwd = vo.getPwd();
+
+		String newPwd = enc.encode(pwd);
+
+		vo.setPwd(newPwd);
+
 		return dao.join(vo, sst);
 	}
 
-	//아이디 중복확인
+	// 아이디 중복확인
 	public int checkId(String id) {
 		// conn : 이미 지역변수 sst 있음.
 		// sql : dao에게 보내기.
@@ -40,15 +50,24 @@ public class MemberService {
 		// tx || rs : tx는 자동처리 / rs는 mybatis가 처리해줌.
 		// close : spring이 처리해줌
 	}
-	
-	//로그인
+
+	// 로그인
 	public MemberVo login(MemberVo memberVo) {
 
-		return dao.login(sst, memberVo);
+		MemberVo memberLog = dao.login(sst, memberVo);
+		String userPwd = memberVo.getPwd();
+		String dbPwd = memberLog.getPwd();
+		boolean result = enc.matches(userPwd, dbPwd);
+		if (result) {
+			return memberLog;
+		} else {
+			return null;
+		}
+
 	}
 
-	//회원정보 수정
-	public MemberVo myInfo(MemberVo vo)throws Exception {
+	// 회원정보 수정
+	public MemberVo myInfo(MemberVo vo) throws Exception {
 
 		int result = dao.myInfo(sst, vo);
 		if (result != 1) {
@@ -58,7 +77,7 @@ public class MemberService {
 
 	}
 
-	//my-feed
+	// my-feed
 	public List<MarketVo> getMyMarketFeed(MarketVo marketVo) {
 		return dao.getMyMarketFeed(sst, marketVo);
 	}
@@ -79,7 +98,7 @@ public class MemberService {
 		return dao.getMyFriendFeed(sst, friendVo);
 	}
 
-	//my-help
+	// my-help
 	public List<HelpVo> getMyHelpBoard(HelpVo helpVo, String writeNo) {
 		return dao.getMyHelpBoard(sst, helpVo);
 	}

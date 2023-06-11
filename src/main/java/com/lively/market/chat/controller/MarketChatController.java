4cd29755110c4739e23ad78854147a096f3a3de9
Hye11 <hyewon02711@gmail.com>
2023-06-11 16:24:28 +0900

    chat 수정

--- src/main/java/com/lively/market/chat/controller/MarketChatController.java
+++ src/main/java/com/lively/market/chat/controller/MarketChatController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,8 +28,10 @@ private static final Logger logger = LoggerFactory.getLogger(MarketChatControlle
 	 * home(Locale locale, Model model) { return "login"; }
 	 */
 	
-	@GetMapping("marketChat")
-	public String login(HttpSession session, MemberVo memberVo, Model model, MarketVo marketVo) {
+	@GetMapping("marketChat/{no}")
+	public String login(@PathVariable(required = true) String no, HttpSession session, MemberVo memberVo, Model model, MarketVo marketVo) {
+		
+		session.setAttribute("marketNo", no);
 		
 		session.setAttribute("memberVo", memberVo);
 		session.setAttribute("marketVo", marketVo);
